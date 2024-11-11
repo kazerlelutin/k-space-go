f2e588b73e1501734740b2d1319e2b30779ea57a
Kazerlelutin <b@bouteiller.contact>
2024-11-11 14:10:38 +0100

    test env var

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 go run main.go
+use air on dev
--- utils/db.go
+++ utils/db.go
@@ -1,28 +1,10 @@
 package utils
 
 import (
-	"database/sql"
 	"fmt"
-	"log"
-
-	_ "github.com/lib/pq" // Import du driver PostgreSQL pour CockroachDB
 )
 
 func Db() {
-	connStr := "postgresql://kazerlelutin:ziKUe3adkXPfrjQHUXjZcg@modern-hisser-2843.jxf.gcp-europe-west1.cockroachlabs.cloud:26257/postgres?sslmode=require"
-
-	db, err := sql.Open("postgres", connStr)
-	if err != nil {
-		log.Fatal("Error connecting to the database: ", err)
-	}
-
-	defer db.Close()
-
-	// Vérification de la connexion
-	err = db.Ping()
-	if err != nil {
-		log.Fatal("Could not connect to CockroachDB: ", err)
-	}
 
 	fmt.Println("Connected to CockroachDB successfully!")
 }
